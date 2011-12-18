65dc180d1aecc5ea7ecd61a95b9e347e08903704
Alex 'AdUser' Z <ad_user@mail.ru>
2011-12-18 23:04:26 +1100

    + added check for fwrite()

--- src/ssa.c
+++ src/ssa.c
@@ -743,7 +743,11 @@ get_ssa_uue_data(ssa_uue_data **list, ssa_uue_data **h, char const * const line,
           memcpy(buf, line, 81 * sizeof(char));
           buf[80] = '\n';
           buf[81] = '\0';
-          fwrite(buf, 81, 1, (*h)->data);
+          if (fwrite(buf, 81, sizeof(char), (*h)->data) != 81)
+            {
+              _log(log_warn, MSG_F_WRFAIL);
+              return false;
+            }
           break;
         case UUE_DATA_TAIL :
           fputs(line, (*h)->data);
